ac14032329f5388e2a24343d7e6a7810e32e7486
Oswaldo Hernandez <oswahdez00@gmail.com>
2021-06-08 17:49:39 -0500

    test menu

---
+++ src/pico/build/CMakeFiles/keySTATIC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/keySTATIC.dir/lib/LCDI2C/LCD_I2C.c.obj"
+  "libkeySTATIC.a"
+  "libkeySTATIC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/keySTATIC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/pico/build/CMakeFiles/keypad_4xSTATIC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/keypad_4xSTATIC.dir/lib/LCDI2C/LCD_I2C.c.obj"
+  "libkeypad_4xSTATIC.a"
+  "libkeypad_4xSTATIC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/keypad_4xSTATIC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
